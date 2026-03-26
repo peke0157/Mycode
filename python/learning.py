b4efd96e9be581203e7bce0b96b88da4b96583d0
hiro <hiroto117@Hirotovivobook.localdomain>
2026-03-26 22:30:09 +0900

    learning

--- python/learning.py
+++ python/learning.py
@@ -1,8 +1,6 @@
 import numpy as np
-import librosa
-import librosa.display
 from pathlib import Path
-from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 data_dir = Path('testdata')
 npy_files = list(data_dir.glob('**/*.npy'))
@@ -10,5 +8,23 @@ npy_files = list(data_dir.glob('**/*.npy'))
 X = []
 Y = []
 
-for npy_files in npy_files:
-    mfccs = np.load(npy_files)
+for npy_file in npy_files:
+    mfccs = np.load(npy_file)
+    
+    mean = np.mean(mfccs, axis=1)
+    std = np.std(mfccs, axis=1)
+    concatenate = np.concatenate([mean, std])
+    
+    genru = npy_file.parent.name
+    
+    X.append(concatenate)
+    Y.append(genru)
+    
+X = np.array(X)
+Y = np.array(Y)
+
+print(X.shape)
+print(Y.shape)
+print(np.unique(Y))    
+
+# テスト用データと訓練用データに分ける
